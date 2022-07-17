7b46d87ad775d7119008827e2261c190e93188a7
unknown <mohamedmagdy12@outlook.com>
2022-07-17 14:42:49 +0200

    01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 
 
 import bgImageDesktop from './/assets/images/bg-header-desktop.svg'
 
 function App() {
 
+  
 
   return (
     <>
     <Header src={bgImageDesktop} alt="backgroud image"/>
+    {/* <Joblist job={job}/> */}
     </>
   );
 }
