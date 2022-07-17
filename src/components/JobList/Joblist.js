e68753419dfa65a3c9ba41e7ca98c8c8cd60b0b4
unknown <mohamedmagdy12@outlook.com>
2022-07-17 12:19:12 +0200

    job component

--- src/components/JobList/Joblist.js
+++ src/components/JobList/Joblist.js
@@ -4,10 +4,26 @@ import './joblist.module.css'
 function Job({job}) { 
     return (
         <li>
-            <img alt={job.company} src={job.logo}></img>
-            {job.company}          
-            {job.new && <span>new</span>}  
-            {job.featured && <span>featured</span>}  
+            <picture>
+                <img  alt={job.company} src={job.logo}></img>
+            </picture>
+            <div>
+                <span>{job.company}</span>
+                {job.new && <span className='new'>new</span>}  
+                {job.featured && <span className='featured'>featured</span>}  
+            </div>
+            <h1>{job.position}</h1>
+            <div>
+                <span>{job.postedat}</span>
+                <span>{job.contract}</span>
+                <span>{job.location}</span>
+            </div>
+            <div>
+                <span>{job.role}</span>
+                <span>{job.level}</span>
+                {job.languages.map((lang) => <span>{lang}</span>)}
+                {job.tools.map((tool) => <span>{tool}</span>)}
+            </div>
         </li>
     )
 
