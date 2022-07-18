eefa76ebc25b5ba0d7bcaddb272bdadc99615885
M0hamedMagdy <43698175+M0hamedMagdy@users.noreply.github.com>
2022-07-18 21:32:09 +0200

    styling

--- src/components/JobList/Joblist.js
+++ src/components/JobList/Joblist.js
@@ -1,43 +1,49 @@
-import './joblist.module.css'
+import styles from "./joblist.module.css";
 
+function Job({ job }) {
+  return (
+    <li className={job.featured && `${styles.featured}`}>
+      <picture>
+        <img alt={job.company} src={job.logo}></img>
+      </picture>
+      <div className={styles.jobInfo}>
+        <div>
+          <span>{job.company}</span>
+          {job.new && <span className={styles.new}>New</span>}
+          {job.featured && <span className={styles.featured}>Featured</span>}
+        </div>
+        <h1>{job.position}</h1>
+        <div>
+          <span>{job.postedAt}</span>
+          <span>{job.contract}</span>
+          <span>{job.location}</span>
+        </div>
+      </div>
+      <div className={styles.tags}>
+        <span>{job.role}</span>
 
-function Job({job}) { 
-    return (
-        <li>
-            <picture>
-                <img  alt={job.company} src={job.logo}></img>
-            </picture>
-            <div>
-                <span>{job.company}</span>
-                {job.new && <span className='new'>new</span>}  
-                {job.featured && <span className='featured'>featured</span>}  
-            </div>
-            <h1>{job.position}</h1>
-            <div>
-                <span>{job.postedat}</span>
-                <span>{job.contract}</span>
-                <span>{job.location}</span>
-            </div>
-            <div>
-                <span>{job.role}</span>
-                <span>{job.level}</span>
-                {job.languages.map((lang) => <span>{lang}</span>)}
-                {job.tools.map((tool) => <span>{tool}</span>)}
-            </div>
-        </li>
-    )
+        <span>{job.level}</span>
 
+        {job.languages.map((lang) => (
+          <span>{lang}</span>
+        ))}
+
+        {job.tools.map((tool) => (
+          <span>{tool}</span>
+        ))}
+      </div>
+    </li>
+  );
 }
 
-
-
-function Joblist({jobs}) { 
-    return(
-        <ul>
-            {jobs.map((job) => <Job key={job.id} job={job} />)}
-        </ul>
-    )
+function Joblist({ jobs }) {
+  return (
+    <ul>
+      {jobs.map((job) => (
+        <Job key={job.id} job={job} />
+      ))}
+    </ul>
+  );
 }
-
 
 export default Joblist;
