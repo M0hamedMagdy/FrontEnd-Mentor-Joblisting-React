104f315117ea929c03715b801ff05bc4dcdfa40f
unknown <mohamedmagdy12@outlook.com>
2022-07-20 09:59:38 +0200

    build

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
